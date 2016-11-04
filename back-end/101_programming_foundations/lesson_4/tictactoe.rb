2c1a67629e4cbd5652bd3b517f6f8a928e836fa2
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2016-11-04 09:20:13 -0500

    Completed TTT bonus features

--- back-end/101_programming_foundations/lesson_4/tictactoe.rb
+++ back-end/101_programming_foundations/lesson_4/tictactoe.rb
@@ -6,6 +6,7 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
 INITIAL_MARKER = ' '.freeze
 PLAYER_MARKER = 'X'.freeze
 COMPUTER_MARKER = 'O'.freeze
+FIRST_MOVE = 'choose'.freeze
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -40,6 +41,22 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def place_piece!(brd, current_player)
+  if current_player == 'player'
+    player_places_piece!(brd)
+  elsif current_player == 'computer'
+    computer_places_piece!(brd)
+  end
+end
+
+def alternate_player(current_player)
+  if current_player == 'player'
+    'computer'
+  elsif current_player == 'computer'
+    'player'
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -53,8 +70,44 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = if winning_play?(brd)
+             winning_square(brd)
+           elsif immediate_threat?(brd)
+             threatened_square(brd)
+           elsif brd[5] == INITIAL_MARKER
+             5
+           else
+             empty_squares(brd).sample
+           end
   brd[square] = COMPUTER_MARKER
+end
+
+def winning_play?(brd)
+  !!winning_square(brd)
+end
+
+def winning_square(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(COMPUTER_MARKER) == 2 &&
+       brd.values_at(*line).count(INITIAL_MARKER) == 1
+      return line[brd.values_at(*line).index(INITIAL_MARKER)]
+    end
+  end
+  nil
+end
+
+def immediate_threat?(brd)
+  !!threatened_square(brd)
+end
+
+def threatened_square(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 2 &&
+       brd.values_at(*line).count(INITIAL_MARKER) == 1
+      return line[brd.values_at(*line).index(INITIAL_MARKER)]
+    end
+  end
+  nil
 end
 
 def board_full?(brd)
@@ -69,6 +122,28 @@ def detect_winner(brd)
   WINNING_LINES.each do |line|
     return 'Player' if brd.values_at(*line).count(PLAYER_MARKER) == 3
     return 'Computer' if brd.values_at(*line).count(COMPUTER_MARKER) == 3
+  end
+  nil
+end
+
+def someone_won_series?(player_score, computer_score)
+  !!detect_series_winner(player_score, computer_score)
+end
+
+def update_score(player_score, computer_score, winner)
+  if winner == 'Player'
+    player_score += 1
+  elsif winner == 'Computer'
+    computer_score += 1
+  end
+  [player_score, computer_score]
+end
+
+def detect_series_winner(player_score, computer_score)
+  if player_score == 5
+    return 'Player'
+  elsif computer_score == 5
+    return 'Computer'
   end
   nil
 end
@@ -88,28 +163,63 @@ def joinor(arr, delimeter = ', ', word = 'or')
   choices
 end
 
-loop do
-  board = initialize_board
+loop do # main loop
+  player_score = 0
+  computer_score = 0
 
-  loop do
+  loop do # play_again loop
+    board = initialize_board
+    current_player = ''
+
+    if FIRST_MOVE == 'choose'
+      loop do
+        prompt "Who should go first: player (P) or computer (C)?"
+        first_input = gets.chomp
+
+        current_player = if first_input.downcase.start_with?('p')
+                  'player'
+                elsif first_input.downcase.start_with?('c')
+                  'computer'
+                end
+
+        break if first_input.downcase.start_with?('p', 'c')
+        prompt "That's not a valid choice."
+      end
+    elsif FIRST_MOVE == 'player'
+      current_player = 'player'
+    elsif FIRST_MOVE == 'computer'
+      current_player = 'computer'
+    end
+
+    loop do
+      display_board(board)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
+      break if someone_won?(board) || board_full?(board)
+    end
+
     display_board(board)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    if someone_won?(board)
+      prompt "#{detect_winner(board)} won the game!"
+      player_score, computer_score =
+        update_score(player_score, computer_score, detect_winner(board))
+    else
+      prompt "It's a tie!"
+    end
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    prompt "Score -- Player: #{player_score}; Computer: #{computer_score}"
+
+    if someone_won_series?(player_score, computer_score)
+      prompt "#{detect_series_winner(player_score, computer_score)} won the series!"
+      break
+    end
+
+    prompt "Hit ENTER to start next game in series."
+    gets.chomp
   end
 
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
-  prompt "Do you want to play again?"
+  prompt "Do you want to play another series?"
   play_again = gets.chomp
   break unless play_again.downcase.start_with?('y')
 end
