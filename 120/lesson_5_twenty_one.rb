ff7b801d82aaea15b6fbe0064b92b31f176eebd4
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2017-01-05 11:36:21 -0600

    Created Hand class

--- 120/lesson_5_twenty_one.rb
+++ 120/lesson_5_twenty_one.rb
@@ -18,50 +18,30 @@ class Deck
     cards
   end
 
-  def deal_two_cards(player)
-    2.times { player.hand << cards.delete(cards.sample) }
+  def deal(player, number_of_cards = 1)
+    number_of_cards.times do
+      player.hand.cards << cards.delete(cards.sample)
+    end
   end
 end
 
-class Participant
-  BUST_TOTAL = 21
-
-  attr_accessor :hand
+class Hand
+  attr_reader :cards
 
   def initialize
-    @hand = []
+    @cards = []
   end
 
-  def busted?
-    hand_value > BUST_TOTAL
-  end
+  def value(hide_card = false)
+    eval_hand = hide_card ? sort_aces(cards_first_hidden) : sort_aces(cards)
 
-  def hide_first_card
-    hidden_hand = hand.clone
-    hidden_hand[0] = ['?', '?']
-    hidden_hand
-  end
-
-  def hit(deck)
-    hand << deck.cards.delete(deck.cards.sample)
-  end
-
-  def display_hand(options = { hide_first_card: false })
-    if options[:hide_first_card]
-      puts "#{self.class} Hand: #{hide_first_card}"
-    else
-      puts "#{self.class} Hand: #{hand} --> #{hand_value}"
-    end
-  end
-
-  def hand_value
     total = 0
-    sort_aces_for_evaluation.each do |card|
+    eval_hand.each do |card|
       case card[0]
       when 'J', 'Q', 'K'
         total += 10
       when 'A'
-        total += 11
+        total += determine_ace_value(total)
       else
         total += card[0].to_i
       end
@@ -69,11 +49,24 @@ class Participant
     total
   end
 
+  def cards_first_hidden
+    cards_first_hidden = cards.drop(1)
+    cards_first_hidden.unshift(['?', '?'])
+  end
+
+  def display(hide_card = false)
+    if hide_card
+      cards_first_hidden
+    else
+      cards
+    end
+  end
+
   private
 
-  def sort_aces_for_evaluation
-    eval_hand = hand.clone
-    hand.each do |card|
+  def sort_aces(cards)
+    eval_hand = cards.clone
+    cards.each do |card|
       if card[0] == 'A'
         eval_hand.delete(card)
         eval_hand.push(card)
@@ -82,23 +75,33 @@ class Participant
     eval_hand
   end
 
-  def determine_ace_value(hand_value)
-    hand_value + 11 > BUST_TOTAL ? 1 : 11
+  def determine_ace_value(value)
+    value + 11 > Participant::BUST_TOTAL ? 1 : 11
   end
 end
 
-class Player < Participant
-end
+class Player
+  BUST_TOTAL = 21
 
-class Dealer < Participant
+  attr_accessor :hand
+
+  def initialize
+    @hand = Hand.new
+  end
+
+  def busted?
+    hand.value > BUST_TOTAL
+  end
 end
 
 class TwentyOne
   DEALER_STAY_TOTAL = 17
 
+  attr_accessor :deck, :player, :dealer
+
   def initialize
     @player = Player.new
-    @dealer = Dealer.new
+    @dealer = Player.new
     @deck = Deck.new
   end
 
@@ -108,30 +111,27 @@ class TwentyOne
   end
 
   def display_goodbye_message
+    puts ""
     puts "Thanks for playing!"
   end
 
   def deal_cards
     print "Hit Enter to deal cards!"
     gets
-    @deck.deal_two_cards(@player)
-    @deck.deal_two_cards(@dealer)
+    deck.deal(player, 2)
+    deck.deal(dealer, 2)
   end
 
-  def display_hands(options = { hide_dealer_card: false })
+  def display_hands(hide_card = false)
     puts ""
-    if options[:hide_dealer_card]
-      @dealer.display_hand(hide_first_card: true)
-    else
-      @dealer.display_hand
-    end
-    @player.display_hand
+    puts "Dealer Hand: #{dealer.hand.display(hide_card)} --> #{dealer.hand.value(hide_card)}"
+    puts "Player Hand: #{player.hand.display} --> #{player.hand.value}"
     puts ""
   end
 
   def player_turn
     loop do
-      display_hands(hide_dealer_card: true)
+      display_hands(true)
       answer = nil
 
       loop do
@@ -141,14 +141,14 @@ class TwentyOne
         puts "That's not a valid choice!"
       end
 
-      @player.hit(@deck) if answer == 'h'
-      break if answer == 's' || @player.busted?
+      deck.deal(player) if answer == 'h'
+      break if answer == 's' || player.busted?
     end
   end
 
   def display_player_result
-    if @player.busted?
-      display_hands
+    if player.busted?
+      display_hands(true)
       puts "You busted! Dealer wins."
     else
       puts "You stayed. It's the dealer's turn."
@@ -158,19 +158,19 @@ class TwentyOne
   def dealer_turn
     loop do
       display_hands
-      break if @dealer.hand_value >= DEALER_STAY_TOTAL
-      sleep 3
-      @dealer.hit(@deck)
+      break if dealer.hand.value >= DEALER_STAY_TOTAL
       puts "Dealer hits."
+      sleep 3
+      deck.deal(dealer)
     end
   end
 
   def display_dealer_result
-    if @dealer.busted?
+    if dealer.busted?
       puts "Dealer busted! You win!"
-    elsif @player.hand_value > @dealer.hand_value
+    elsif player.hand.value > dealer.hand.value
       puts "Dealer must stay! You win!"
-    elsif @player.hand_value < @dealer.hand_value
+    elsif player.hand.value < dealer.hand.value
       puts "Dealer wins!"
     else
       puts "It's a tie!"
@@ -183,7 +183,7 @@ class TwentyOne
     deal_cards
     player_turn
     display_player_result
-    unless @player.busted?
+    unless player.busted?
       dealer_turn
       display_dealer_result
     end
