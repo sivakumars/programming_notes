dd21d6211212fe7c186421944e763df709cd984c
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2017-01-11 09:18:07 -0600

    Completed foundations easy2 exercises

--- exercises/foundations_easy_2.rb
+++ exercises/foundations_easy_2.rb
@@ -181,48 +181,46 @@ system 'clear'
 
 ## Exercise 10
 
-def each_cons(array, num_elements)
-  index = 0
-
-  while index <= array.size - num_elements
-    elements = array[(index + 1), num_elements - 1]
-    yield(array[index], *elements)
-    index += 1
-  end
-  nil
-end
-
-hash = {}
-each_cons([1, 3, 6, 10], 1) do |value|
-  hash[value] = true
-end
-p hash
-p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
-
-hash = {}
-each_cons([1, 3, 6, 10], 2) do |value1, value2|
-  hash[value1] = value2
-end
-p hash
-p hash == { 1 => 3, 3 => 6, 6 => 10 }
-
-hash = {}
-each_cons([1, 3, 6, 10], 3) do |value1, *values|
-  hash[value1] = values
-end
-p hash
-p hash == { 1 => [3, 6], 3 => [6, 10] }
-
-hash = {}
-each_cons([1, 3, 6, 10], 4) do |value1, *values|
-  hash[value1] = values
-end
-p hash
-p hash == { 1 => [3, 6, 10] }
-
-hash = {}
-each_cons([1, 3, 6, 10], 5) do |value1, *values|
-  hash[value1] = values
-end
-p hash
+# def each_cons(array, n)
+#   index = 0
+#   while index <= array.size - n
+#     elements = array[(index + 1), n - 1]
+#     yield(array[index], *elements)
+#     index += 1
+#   end
+# end
+#
+# hash = {}
+# each_cons([1, 3, 6, 10], 1) do |value|
+#   hash[value] = true
+# end
+# p hash
+# p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+#
+# hash = {}
+# each_cons([1, 3, 6, 10], 2) do |value1, value2|
+#   hash[value1] = value2
+# end
+# p hash
+# p hash == { 1 => 3, 3 => 6, 6 => 10 }
+#
+# hash = {}
+# each_cons([1, 3, 6, 10], 3) do |value1, *values|
+#   hash[value1] = values
+# end
+# p hash
+# p hash == { 1 => [3, 6], 3 => [6, 10] }
+#
+# hash = {}
+# each_cons([1, 3, 6, 10], 4) do |value1, *values|
+#   hash[value1] = values
+# end
+# p hash
+# p hash == { 1 => [3, 6, 10] }
+#
+# hash = {}
+# each_cons([1, 3, 6, 10], 5) do |value1, *values|
+#   hash[value1] = values
+# end
+# p hash
 p hash == {}
