9d2bac44ab6c1654255dfb51be9a8e5fd280484d
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2017-02-16 17:20:09 -0600

    Completed up through testing w/ session variables

--- 170/cms/cms.rb
+++ 170/cms/cms.rb
@@ -1,8 +1,128 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "redcarpet"
 
+configure do
+  enable :sessions
+  set :session_secret, 'super secret'
+end
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("test/data")
+  else
+    File.expand_path("data")
+  end
+end
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
+
+def valid_user?(username, password)
+  username == "admin" && password == "secret"
+end
 
 get "/" do
-  "Getting started."
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
+    File.basename(path)
+  end
+  erb :index
+end
+
+get "/users/signin" do
+  erb :signin
+end
+
+post "/users/signin" do
+  username = params[:username]
+  password = params[:password]
+
+  if valid_user?(username, password)
+    session[:username] = username
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials."
+    status 422
+    erb :signin
+  end
+end
+
+post "/users/signout" do
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+  redirect "/"
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/create" do
+  filename = params[:filename]
+
+  if filename.size == 0
+    session[:message] = "A name is required."
+    status 422
+    erb :new
+  else
+    file_path = File.join(data_path, filename)
+
+    File.write(file_path, "")
+    session[:message] = "#{filename} has been created."
+    redirect "/"
+  end
+end
+
+post "/:filename/delete" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.delete(file_path)
+
+  session[:message] = "#{params[:filename]} has been deleted."
+  redirect "/"
+end
+
+get "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  unless File.file?(file_path)
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
+
+  load_file_content(file_path)
+end
+
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
+  @filename = params[:filename]
+  @content = File.read(file_path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
 end
---
+++ 170/cms/test/cms_test.rb
@@ -0,0 +1,162 @@
+ENV["RACK_ENV"] = "test"
+
+system 'clear'
+require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
+require "rack/test"
+require "fileutils"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.write(File.join(data_path, name), content)
+  end
+
+  def session
+    last_request.env["rack.session"]
+  end
+
+  def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
+    get "/"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.md"
+    assert_includes last_response.body, "changes.txt"
+  end
+
+  def test_viewing_text_document
+    create_document "history.txt", "2003 - Ruby 1.8 released."
+    get "/history.txt"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "2003 - Ruby 1.8 released."
+  end
+
+  def test_document_not_found
+    get "/notafile.ext"
+    assert_equal 302, last_response.status
+    assert_equal "notafile.ext does not exist.", session[:message]
+  end
+
+  def test_viewing_markdown_document
+    create_document "about.md", "# Ruby is..."
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
+  end
+
+  def test_editing_document
+    create_document "changes.txt"
+
+    get "/changes.txt/edit"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_updating_document
+    post "/changes.txt", content: "new content"
+
+    assert_equal 302, last_response.status
+    assert_equal "changes.txt has been updated.", session[:message]
+
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
+  end
+
+  def test_view_new_document_form
+    get "/new"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body,  %q(<button type="submit")
+  end
+
+  def test_create_new_document
+    post "/create", filename: "test.txt"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_includes last_response.body, "test.txt has been created."
+
+    get "/"
+    assert_includes last_response.body, "test.txt"
+  end
+
+  def test_create_new_document_without_filename
+    post "/create", filename: ""
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, "A name is required."
+  end
+
+  def test_deleting_document
+    doc = "test.txt"
+    create_document doc
+
+    post "/#{doc}/delete"
+    assert_equal 302, last_response.status
+    assert_equal "#{doc} has been deleted.", session[:message]
+
+    get "/"
+    refute_includes last_response.body, %q(href="/#{doc})
+  end
+
+  def test_signin_form
+    get "/users/signin"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body,  %q(<button type="submit")
+  end
+
+  def test_signin
+    post "/users/signin", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+    assert_equal "Welcome!", session[:message]
+    assert_equal "admin", session[:username]
+
+    get last_response["Location"]
+    assert_includes last_response.body, "Signed in as admin."
+  end
+
+  def test_signin_with_bad_credentials
+    post "/users/signin", username: "admin", password: "bummer"
+    assert_equal 422, last_response.status
+    assert_nil session[:username]
+    assert_includes last_response.body, "Invalid credentials."
+  end
+
+  def test_signout
+    post "/users/signout", {}, {"rack.session" => { username: "admin"} }
+    get last_response["Location"]
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "You have been signed out."
+    assert_includes last_response.body, "Sign In"
+  end
+end
